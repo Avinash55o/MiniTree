9d015d810a0722c60e7c161bad08cb387cf12829
avinash55o <avinashboruah8@gmail.com>
2025-12-18 23:04:51 +0530

    added link router in the index file

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import { authRouter } from './routes/auth.route.js';
 import { initDB } from './db/index.js';
-
+import { linksRouter } from './routes/links.route.js';
 
 const app = express();
 app.use(express.json());
 
 app.use('/auth', authRouter)
+app.use('/link',linksRouter)
 
 initDB().then(() => {
     app.listen(8080, () => {
