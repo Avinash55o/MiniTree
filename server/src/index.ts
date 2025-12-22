0854670a9164d738f871c6c20b333724d27a668b
Priyanku Gogoi <workwithpriyanku@gmail.com>
2025-12-22 13:06:10 +0530

    corrected route + added cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 import { authRouter } from './routes/auth.route.js';
 import { initDB } from './db/index.js';
 import { linksRouter } from './routes/links.route.js';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use('/auth', authRouter)
-app.use('/link',linksRouter)
+app.use('/link', linksRouter)
 
 initDB().then(() => {
     app.listen(8080, () => {
